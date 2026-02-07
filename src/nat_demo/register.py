34e9eb3131b8bf2c3fc5b026b41a401cb4ba3508
Uladzislau Kiyeuski <vladislavki@playtika.com>
2026-02-08 00:06:31 +0100

    Create demo project

---
+++ src/nat_demo/register.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# Import the generated workflow function to trigger registration
+from nat_demo.weather_tool import weather_tool_function
+from nat_demo.langgraph_time_agent import langgraph_time_agent_function
